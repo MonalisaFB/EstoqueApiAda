808f7ba3f2d3bed17622c46afe6a06faa68c100f
Monalisa Freitas de Brito <monalisabrito.ads@gmail.com>
2024-03-04 11:31:02 -0300

    Teste Regitrar item

--- EstoqueTest/TestController/ProdutosTestController.cs
+++ EstoqueTest/TestController/ProdutosTestController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using EstoqueTest.MockData;
+using EstoqueTest.Models;
+using FluentAssertions;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +13,32 @@ namespace EstoqueTest.TestController
     public class ProdutosTestController
     {
         [Fact]
-        public void GetProdutos_RetornaListaDeProdutos()
+        public void RegistrarEntrada_Item_Sucesso()
         {
             //Arrange
+            var produtoRepositorioMock = new ProdutoRepositoryMock();
+            var service = new ProdutoServiceMock(produtoRepositorioMock);
+            var novoProduto = new Produto
+            {
+                ProdutoId = 2,
+                Descricao = "Suco",
+                Quantidade = 5,
+                DataFabricacao = DateTime.Now,
+                DateValidade = DateTime.Now.AddDays(1)
+            };
+
             //Act
+            service.AdicionarProduto(novoProduto);
+
             //Assert
+            var produtos = service.ObterProdutos();
+            produtos.Should().ContainEquivalentOf(novoProduto);
+
+            var quantidadeTotal = produtos.
+                                    Where(x => x.ProdutoId == novoProduto.ProdutoId).Sum(y => y.Quantidade);
+            quantidadeTotal.Should().Be(45);
         }
 
-        [Th]
+       
     }
 }
