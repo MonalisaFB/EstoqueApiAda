3afaec9f4559dbbb36e630549d0e0ed72179f39a
Monalisa Freitas de Brito <monalisabrito.ads@gmail.com>
2024-03-04 10:29:00 -0300

    ProdutoServiceMock

--- EstoqueTest/MockData/ProdutoServiceMock.cs
+++ EstoqueTest/MockData/ProdutoServiceMock.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EstoqueTest.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,31 @@ namespace EstoqueTest.MockData
 {
     public class ProdutoServiceMock
     {
+        private readonly ProdutoRepositoryMock _produtoRepositoryMock;
+
+        public ProdutoServiceMock(ProdutoRepositoryMock produtoRepositoryMock)
+        {
+            _produtoRepositoryMock = produtoRepositoryMock;
+        }
+
+        public IEnumerable<Produto> ObterProdutos()
+        {
+            return _produtoRepositoryMock.ObterProdutos();
+        }
+
+        public void AdicionarProduto(Produto produto)
+        {
+            _produtoRepositoryMock.AdicionarProduto(produto);
+        }
+
+        public void AtualizarProduto(Produto produto)
+        {
+            _produtoRepositoryMock.AdicionarProduto(produto);
+        }
+
+        public void RemoverProduto(int id)
+        {
+            _produtoRepositoryMock.RemoverProduto(id);
+        }
     }
 }
